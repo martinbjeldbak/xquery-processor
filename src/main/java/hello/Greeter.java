854f97add1660a6a9c19d185a2477550b7196c0a
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-08 14:46:25 -0700

    Add dummy source files for now

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by martin on 07/04/15.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+
+/**
+ * Created by martin on 07/04/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+
+        System.out.println(greeter.sayHello());
+    }
+}
