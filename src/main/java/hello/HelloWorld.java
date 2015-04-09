d32ff0cc6c5f347761b2dd01c0f0ffb7aa5d0b7a
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-08 20:00:21 -0700

    Working with jdom2

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,5 +1,13 @@
 package hello;
 
+import java.io.File;
+import java.util.List;
+
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 /**
  * Created by martin on 07/04/15.
@@ -7,7 +15,24 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
+        SAXBuilder builder = new SAXBuilder();
 
         System.out.println(greeter.sayHello());
+
+        File caesarF = new File("samples/j_caesar.xml");
+
+        try {
+            Document document = (Document) builder.build(caesarF);
+            Element rootNode = document.getRootElement();
+
+
+            System.out.println(rootNode.getChildText("TITLE"));
+
+
+
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
