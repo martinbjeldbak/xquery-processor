c11b5f0309fa6b8d9179e77b61229ab099c889b7
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-15 20:50:13 -0700

    Finally get IDEA to recognize antlr-generated src files.

--- src/main/java/dk/martinbmadsen/xquery/main/Main.java
+++ src/main/java/dk/martinbmadsen/xquery/main/Main.java
@@ -1,14 +1,21 @@
 package dk.martinbmadsen.xquery.main;
 
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
 /**
  * Created by martin on 09/04/15.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 
     public int giveTwo() {
         return 2;
     }
+
+
+
 }
