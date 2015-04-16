8ef04f83e3411a4beb2ccb77851c64e535825cd2
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-15 22:11:58 -0700

    Move generation ANTLR packages to target/generated-sources. They can still be imported!! :D

--- src/main/java/dk/martinbmadsen/xquery/main/Main.java
+++ src/main/java/dk/martinbmadsen/xquery/main/Main.java
@@ -2,6 +2,7 @@ package dk.martinbmadsen.xquery.main;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import dk.martinbmadsen.xquery.parser.*;
 
 /**
  * Created by martin on 09/04/15.
@@ -9,6 +10,8 @@ import org.antlr.v4.runtime.tree.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //HelloLexer hl = new HelloLexer();
     }
 
     public int giveTwo() {
