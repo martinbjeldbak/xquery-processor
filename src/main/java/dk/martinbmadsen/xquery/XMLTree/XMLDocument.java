7de223319a42479e806d9f4522eb9c4bb6aba660
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-17 01:16:09 -0700

    Add XMLDocument wrapper class around JDOM2 document

---
+++ src/main/java/dk/martinbmadsen/xquery/XMLTree/XMLDocument.java
@@ -0,0 +1,27 @@
+package dk.martinbmadsen.xquery.XMLTree;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.input.sax.XMLReaders;
+
+import java.io.IOException;
+
+public class XMLDocument {
+    private Document doc;
+
+    public XMLDocument(String fileName) {
+        SAXBuilder sax = new SAXBuilder(XMLReaders.DTDVALIDATING);
+
+        try {
+            this.doc = sax.build(fileName);
+        } catch (JDOMException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Element root() {
+        return doc.getRootElement();
+    }
+}
