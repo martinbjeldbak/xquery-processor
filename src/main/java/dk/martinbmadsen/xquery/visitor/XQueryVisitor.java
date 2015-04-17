8b7e5ea0716aafdac174533dcaee4e272fa8e1e3
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-17 01:16:31 -0700

    Match which slash is given as input

--- src/main/java/dk/martinbmadsen/xquery/visitor/XQueryVisitor.java
+++ src/main/java/dk/martinbmadsen/xquery/visitor/XQueryVisitor.java
@@ -1,16 +1,36 @@
 package dk.martinbmadsen.xquery.visitor;
 
+import dk.martinbmadsen.xquery.XMLTree.XMLDocument;
 import dk.martinbmadsen.xquery.parser.XQueryBaseVisitor;
+import dk.martinbmadsen.xquery.parser.XQueryLexer;
 import dk.martinbmadsen.xquery.parser.XQueryParser;
 import org.antlr.v4.runtime.misc.NotNull;
 
-/**
- * Created by martin on 16/04/15.
- */
-public class XQueryVisitor extends XQueryBaseVisitor {
+public class XQueryVisitor<T> extends XQueryBaseVisitor<T> {
+
     @Override
-    public Object visitAp(@NotNull XQueryParser.ApContext ctx) {
-        System.out.println(ctx.getText());
+    public T visitAp(@NotNull XQueryParser.ApContext ctx) {
+        String fileName = ctx.fileName.getText().replace("'", "");
+
+        XMLDocument document = new XMLDocument(fileName);
+
+
+
+        visit(ctx.rp());
+
+        switch(ctx.slash.getType()) {
+            case XQueryLexer.SLASH:
+                System.out.println("Matched /");
+                break;
+            case XQueryLexer.SSLASH:
+                System.out.println("Matched //");
+                break;
+            default:
+                System.out.println("Oops, shouldn't be here");
+                break;
+        }
+
+
         return super.visitAp(ctx);
     }
 }
