c878b58b118b62b4fc4adc8789081fa046c320cc
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-19 13:52:30 -0700

    Add Document Interface, make XMLDocument implement it

---
+++ src/main/java/dk/martinbmadsen/xquery/XMLTree/IXMLDocument.java
@@ -0,0 +1,9 @@
+package dk.martinbmadsen.xquery.XMLTree;
+
+public interface IXMLDocument {
+    /**
+     * The root of the XML tree corresponding to this document
+     * @return the root element
+     */
+    IXMLElement root();
+}
--- src/main/java/dk/martinbmadsen/xquery/XMLTree/XMLDocument.java
+++ src/main/java/dk/martinbmadsen/xquery/XMLTree/XMLDocument.java
@@ -8,19 +8,24 @@ import org.jdom2.input.sax.XMLReaders;
 
 import java.io.IOException;
 
-public class XMLDocument {
+public class XMLDocument implements IXMLDocument {
     private Document doc;
 
     public XMLDocument(String fileName) {
         SAXBuilder sax = new SAXBuilder(XMLReaders.DTDVALIDATING);
 
         try {
-            this.doc = sax.build(fileName);
+            this.doc = sax.build(sanitizeFileName(fileName));
         } catch (JDOMException | IOException e) {
             e.printStackTrace();
         }
     }
 
+    private String sanitizeFileName(String fileName) {
+        return fileName.replace("\"", "");
+    }
+
+    @Override
     public IXMLElement root() {
         return new XMLElement(doc.getRootElement());
     }
