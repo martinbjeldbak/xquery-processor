c96b35aee391fbdb25bf1cf21a2165e58701a9b5
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-19 14:30:32 -0700

    Add debug helper functions

--- src/main/java/dk/martinbmadsen/xquery/debug/Debug.java
+++ src/main/java/dk/martinbmadsen/xquery/debug/Debug.java
@@ -2,6 +2,14 @@ package dk.martinbmadsen.xquery.debug;
 
 public class Debug {
     public static void debug(String msg) {
-        System.out.println("DEBUG: " + msg);
+        print("DEBUG: " + msg);
+    }
+
+    public static void result(String msg) {
+        print("RESULT: " + msg);
+    }
+
+    private static void print(String msg) {
+        System.out.println(msg);
     }
 }
