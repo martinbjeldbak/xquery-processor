2468c00c355ae6dc2f8f8fc77dbc36dc91c8b652
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-21 17:01:56 -0700

    Remove unused Error (custom Exception) classes for now

--- src/main/java/dk/martinbmadsen/xquery/error/Error.java
+++
@@ -1,8 +0,0 @@
-package dk.martinbmadsen.xquery.error;
-
-
-public abstract class Error extends Exception implements IError {
-    public Error(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/dk/martinbmadsen/xquery/error/IError.java
+++
@@ -1,9 +0,0 @@
-package dk.martinbmadsen.xquery.error;
-
-/**
- * Created by martin on 18/04/15.
- */
-public interface IError {
-    int getColumn();
-    int getLine();
-}
