265e3404b40946255e081110552e1bd2c9e57912
Thalley <mrthalley@gmail>
2015-06-10 21:47:37 -0700

    Added output to file

--- src/main/java/dk/martinbmadsen/xquery/main/Main.java
+++ src/main/java/dk/martinbmadsen/xquery/main/Main.java
@@ -5,6 +5,7 @@ import dk.martinbmadsen.utils.debug.XQueryExecutor;
 import dk.martinbmadsen.xquery.xmltree.IXMLElement;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +23,27 @@ public class Main {
             e.printStackTrace();
         }
 
-        System.out.println(result.size() + " results below:");
+        if(args.length > 1 && args[1].equals("terminal")){
+            System.out.println(result.size() + " results below:");
             Integer i = 0;
             for(IXMLElement c : result) {
                 Debugger.result("#" + i++);
                 System.out.println(c.toString());
             }
+        }
+        else{
+            try{
+                PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+                for(IXMLElement c : result) {
+                    writer.println(c.toString());
+                }
+                writer.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
